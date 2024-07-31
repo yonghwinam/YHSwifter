49e1ee676e4cc1f3b73bc6da01196dfdff09381d
Yonghwi <yonghwinam@smtown.com>
2024-07-31 14:19:47 +0900

    fix test resize image

--- Tests/YHSwifterTests/YHUIKitExtenstionTests.swift
+++ Tests/YHSwifterTests/YHUIKitExtenstionTests.swift
@@ -12,16 +12,16 @@ import UIKit
 
 struct YHUIKitExtenstionTests {
 
-    @Test func UIImage_resize() async throws {
+    @Test(arguments: ["img1.jpg", "img2.jpg", "img3.jpg"])
+    func UIImage_resize(imgName: String) async throws {
         let otherSize = CGSize(width: 640, height: 480)
         
-        let uiImage = UIImage(named: "img1.jpg", in: Bundle.module, compatibleWith: nil)
-        YHDebugLog(uiImage?.size)
+        let uiImage = UIImage(named: imgName, in: Bundle.module, compatibleWith: nil)
+        YHDebugLog("origin size: \(uiImage!.size)")
         
         let resizedImage = uiImage?.resized(to: otherSize)
-        YHDebugLog(resizedImage?.size)
+        YHDebugLog("resized image size: \(resizedImage!.size)")
         
         #expect(resizedImage?.size == otherSize)
     }
-
 }
